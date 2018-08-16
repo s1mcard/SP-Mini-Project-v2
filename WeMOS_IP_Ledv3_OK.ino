d2d65841a21d9698666e248e64693342f694fd16
s1mcard <42053991+s1mcard@users.noreply.github.com>
2018-08-16 22:11:30 +0800

    Add files via upload

---
+++ WeMOS_IP_Ledv3_OK.ino
@@ -0,0 +1,134 @@
+/*
+http://www.esp8266learning.com/wemos-webserver-example.php
+USB-SERIAL CH340 (COM23)
+*/
+
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "12-219";
+const char* password = "12348765";
+
+//const char* ssid = "E5830-5784-ABC";
+//const char* password = "37714601";
+
+
+//const char* ssid = "APn22";
+//const char* password = "A120506p";
+
+
+//const char* ssid     = "eee-iot";
+//const char* password = "howIknow@07";
+ 
+int ledPin = D5;
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.println("WeNOS_IP_ledv3.ino");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL : ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1) {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  } 
+  if (request.indexOf("/LED=OFF") != -1){
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+ 
+ 
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.print("SSID is ");
+  client.print(ssid); 
+  client.println("<br>");
+
+  client.print("IP is ");
+  client.print(WiFi.localIP());
+  client.println("<br>");
+  client.print("Led pin is now: ");
+   
+  if(value == HIGH) {
+    client.print("On");  
+  } else {
+    client.print("Off");
+  }
+  
+  client.println("<br>");
+  client.println("Click <a href=\"/LED=ON\">here</a> turn the LED on pin 5 ON<br>");
+  client.println("Click <a href=\"/LED=OFF\">here</a> turn the LED on pin 5 OFF<br>");
+  client.println("</html>");
+ 
+  delay(10);
+  Serial.println("Client disconnected");
+  Serial.println("");
+ 
+}
+
+void blinkLED () {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+                                    // but actually the LED is on; this is because 
+                                    // it is active low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
+
